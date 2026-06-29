e275e6778d095c17325f2309d72ea4f72200e1c3
EricLima1993 <merick_lima@hotmail.com>
2026-06-28 23:04:20 -0300

    Ajustes na geração de PDFs, nova biblioteca complementar para JasperReports e finalização do fluxo da nota fiscal

--- src/main/java/io/github/ericlima1993/icompras/faturamento/service/GeradorNotaFiscalService.java
+++ src/main/java/io/github/ericlima1993/icompras/faturamento/service/GeradorNotaFiscalService.java
@@ -1,14 +1,33 @@
 package io.github.ericlima1993.icompras.faturamento.service;
 
+import io.github.ericlima1993.icompras.faturamento.bucket.BucketFile;
 import io.github.ericlima1993.icompras.faturamento.model.Pedido;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class GeradorNotaFiscalService {
 
+    private final NotaFiscalService notaFiscalService;
+    private final BucketService bucketService;
+
     public void gerar(Pedido pedido) {
-        log.info("Gerada a nota fiscal do pedido: {}", pedido.codigo());
+        log.info("Gerando a nota fiscal do pedido: {}", pedido.codigo());
+        try{
+            byte[] byteArray = notaFiscalService.gerarNota(pedido);
+            String nomeArquivo = String.format("nota_fiscal_pedido_%d.pdf", pedido.codigo());
+            var file = new BucketFile(nomeArquivo, new ByteArrayInputStream(byteArray), MediaType.APPLICATION_PDF, byteArray.length);
+            log.info("Gerada a nota fiscal do pedido: {}, nome do arquivo: {}", pedido.codigo(), nomeArquivo);
+
+            bucketService.upload(file);
+        }catch(Exception e){
+            log.error(e.getMessage(), e);
+        }
     }
 }
